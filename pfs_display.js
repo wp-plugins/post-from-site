42e7cff756be20e402e224747300d9ac4ddf5777
ryelle <ryelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-01 21:18:44 +0000

    1.6.21 fixes the small error disabling js and css
    git-svn-id: https://plugins.svn.wordpress.org/post-from-site/tags/1.6.21@131315 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pfs_display.js
@@ -0,0 +1,9 @@
+function pfsopen(){
+	document.getElementById('pfs_postbox').style.display = 'block';
+	return;
+}
+
+function pfsclose(){
+	document.getElementById('pfs_postbox').style.display = 'none';
+	return;
+}
