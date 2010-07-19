a2ae769f871851e650b27a2c23ee791b1b521129
ryelle <ryelle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-19 02:35:11 +0000

    fixing version number
    git-svn-id: https://plugins.svn.wordpress.org/post-from-site/tags/2.0.1@264482 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post-from-site.php
+++ post-from-site.php
@@ -4,7 +4,7 @@
 * Plugin URI: http://redradar.net/2010/07/post-from-site-v2-0-1/
  * Description: Add a new post directly from your website - no need to go to the admin side.
  * Author: Kelly Dwan
- * Version: 2.0.0
+ * Version: 2.0.1
  * Date: 07.09.10
  * Author URI: http://www.redradar.net/
  */
